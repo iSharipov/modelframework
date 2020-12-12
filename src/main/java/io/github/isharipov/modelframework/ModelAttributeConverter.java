487544393e17b27291f8d2a7bf7b20d2812fbb26
Sharipov Ilia <ilya.sharipov@raiffeisen.ru>
2020-12-12 14:46:44 +0300

    nullability support for attribute converter

--- src/main/java/io/github/isharipov/modelframework/ModelAttributeConverter.java
+++ src/main/java/io/github/isharipov/modelframework/ModelAttributeConverter.java
@@ -6,10 +6,16 @@ import javax.persistence.Converter;
 @Converter(autoApply = true)
 public abstract class ModelAttributeConverter<I extends Model> implements AttributeConverter<Model, String> {
     public String convertToDatabaseColumn(Model attribute) {
+        if (attribute == null) {
+            return null;
+        }
         return attribute.formattedValue();
     }
 
     public Model convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.isEmpty()) {
+            return null;
+        }
         return instance(dbData);
     }
 
